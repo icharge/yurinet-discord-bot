69027e74d0c58f15cd28c71fb199160c136c7873
tmteamz <naiwheatleyth@outlook.com>
2018-03-04 19:31:40 +0700

    1.3.0-Music

--- index.js
+++ index.js
@@ -5,58 +5,71 @@
 */
 // Import REPL for prompt
 const repl = require('repl');
+
 // Import the discord.js module
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({
+  autoReconnect: true
+});
+let yurinetEmbed = new Discord.RichEmbed()
+let gameEmbed = new Discord.RichEmbed()
+let DDwrapperEmbed = new Discord.RichEmbed()
+
 // Import config
 let config = require('./config.json');
 let prefix = config.commandPrefix;
+const chatlog = client.channels.get(config.chatlogidroom);
+
+// Import Music controller
+const musiccontroller = require('./music/musiccontroller.js')
+const music = new musiccontroller(client)
+
 //bot
 client.on('guildMemberAdd', member => {
   console.log(`${member} Has join ${member.guild.name} server.`);
 });
 
-client.on('ready', () => {
+client.once('ready', () => {
   console.log('Im ready!');
   var replServer = repl.start({
     prompt: 'YuriBot > ',
   });
   replServer.context.client = client;
+
+  yurinetEmbed.setAuthor(client.user.username, client.user.avatarURL)
+    .setColor("#EE82EE")
+    .addField("Download",
+      "http://play.thaira2.com/download/yn0910_setup.exe")
+    .addField("เล่นออน์ไลน์ ด้วย yurinet",
+      "ติดตั้งโปรแกรม สมัคร ตั้งค่า แล้วไปลุยกันได้เลย")
+    .setFooter("**แมว")
+
+  gameEmbed.setAuthor(client.user.username, client.user.avatarURL)
+    .setColor("#EE82EE")
+    .addField("Download",
+      "http://www.thaira2.com/download.html")
+    .addField("ตัวเกม red alert 2 yuri revenge",
+      "เลือก link โหลดตัวเกมเพียง link เดี่ยว")
+    .setFooter("**แมว")
+  DDwrapperEmbed.setAuthor(client.user.username, client.user.avatarURL)
+    .setColor("#EE82EE")
+    .addField("Download",
+      "https://drive.google.com/file/d/0B0ELu66No5j9SGpONERnUFFUTHM/view"
+    )
+    .addField("แก้อาการกระตุก หรือ มองไม่เห็นเมนู",
+      "โหลด DDwrapper แล้วนำ 2 ไฟล์ไปลงที่แฟ้มเกมลงแบบเหมือนลงม็อดเกม Yuri"
+    )
+    .setFooter("**ถ้าไม่ทำงานแสดงว่าคุณลงผิดที่ ลองลงใหม่")
 });
 
 client.on('message', async message => {
   if (message.author.bot) return
+  if (message.channel.type != "text") return
   let messageArray = message.content.split(/\s+/g);
   let command = messageArray[0];
   if (command.startsWith(prefix)) {
-    let yurinetEmbed = new Discord.RichEmbed()
-      .setAuthor(client.user.username, client.user.avatarURL)
-      .setColor("#EE82EE")
-      .addField("Download",
-        "http://play.thaira2.com/download/yn0910_setup.exe")
-      .addField("เล่นออน์ไลน์ ด้วย yurinet",
-        "ติดตั้งโปรแกรม สมัคร ตั้งค่า แล้วไปลุยกันได้เลย")
-      .setFooter("**แมว")
-    let gameEmbed = new Discord.RichEmbed()
-      .setAuthor(client.user.username, client.user.avatarURL)
-      .setColor("#EE82EE")
-      .addField("Download",
-        "http://www.thaira2.com/download.html")
-      .addField("ตัวเกม red alert 2 yuri revenge",
-        "เลือก link โหลดตัวเกมเพียง link เดี่ยว")
-      .setFooter("**แมว")
-    let DDwrapperEmbed = new Discord.RichEmbed()
-      .setAuthor(client.user.username, client.user.avatarURL)
-      .setColor("#EE82EE")
-      .addField("Download",
-        "https://drive.google.com/file/d/0B0ELu66No5j9SGpONERnUFFUTHM/view"
-      )
-      .addField("แก้อาการกระตุก หรือ มองไม่เห็นเมนู",
-        "โหลด DDwrapper แล้วนำ 2 ไฟล์ไปลงที่แฟ้มเกมลงแบบเหมือนลงม็อดเกม Yuri"
-      )
-      .setFooter("**ถ้าไม่ทำงานแสดงว่าคุณลงผิดที่ ลองลงใหม่")
     let cmd = command.slice(prefix.length);
-    if (cmd === "DDraw") {
+    if (cmd === "ddraw") {
       message.channel.send({
         embed: DDwrapperEmbed
       });
@@ -84,8 +97,7 @@ socket.on('connect', function(socket) {
   console.log('socket.io-client is connected!');
 });
 let lastChat = {}
-socket.on('lobby.chat', function(response) {
-  const chatlog = client.channels.get(config.chatlogidroom);
+socket.on('lobby.chat', async function(response) {
   let ChatDetail = new Date(response.timestamp);
   let tempChatDate = ChatDetail.getDate();
   let tempChatMonth = ChatDetail.getMonth() + 1;
@@ -121,13 +133,13 @@ socket.on('lobby.chat', function(response) {
     return
   }
   Object.defineProperty(lastChat, response.name, {
-      value: response.message,
-      writable: true,
-      configurable: true
-    })
-    //  console.log(lastChat)
+    value: response.message,
+    writable: true,
+    configurable: true
+  })
+  //  console.log(lastChat)
   chatlog.send(formatChatMessage())
-    //  console.log(response)
+  //  console.log(response)
   console.log(printDiscordChatMessage())
 });
 socket.on('disconnect', function() {
@@ -138,5 +150,5 @@ socket.on('disconnect', function() {
 try {
   client.login(config.token);
 } catch (e) {
-  console.log(e.stack)
+  console.log(e)
 }
